4f534dcc1bbbb5aeb07e47a38c77f191e26d5c17
Patryk Krajewski <patrykraj@interia.pl>
2021-01-11 18:15:38 +0100

    Expandable list of information in forecasts

--- src/components/forecast/Days.js
+++ src/components/forecast/Days.js
@@ -2,52 +2,29 @@ import React from 'react';
 import styled from 'styled-components';
 import propTypes from 'prop-types';
 
-function Day({ data }) {
+import Day from './Day';
+
+function Days({ data }) {
   return (
         <DayList>
             {data.map((day) => (
-                <DayElement key={day.datetime}>
-                    <h4>{day.datetime.slice(-5, day.datetime.length)}</h4>
-                    <img className="day-weather-icon" src={`https://www.weatherbit.io/static/img/icons/${day.weather.icon}.png`} alt='weather-icon' />
-                    <div className="day-temp">
-                        <h4>{Math.round(day.max_temp)}&deg;/</h4>
-                        <h4>{Math.round(day.low_temp)}&deg;</h4>
-                    </div>
-                    <p>Pop: {day.pop}%</p>
-                </DayElement>
+                <Day key={day.datetime} day={day} />
             ))}
         </DayList>
   );
 }
 
-export default Day;
+export default Days;
 
 const DayList = styled.ul`
     padding: 0;
     margin: 0;
     width: 80%;
     max-width: 768px;
+    font-size: 1.2rem;
+    list-style: none;
 `;
 
-const DayElement = styled.ul`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    background: rgba(255,255,255,.2);
-    margin: 10px 0;
-    padding: 0 1rem;
-  
-    .day-temp {
-        display: flex;
-    }
-    
-    .day-weather-icon {
-        width: 16%;
-        min-width: 56px;
-    }
-`;
-
-Day.propTypes = {
+Days.propTypes = {
   data: propTypes.array,
 };
